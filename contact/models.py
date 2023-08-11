224ba7e44f8ae7962cabc178f0d084eac3336390
Lucas Ribeiro <lucasfreitasr@outlook.com>
2023-08-11 13:57:14 -0300

    criando a primeira tabela de contato

---
+++ contact/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=250, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
