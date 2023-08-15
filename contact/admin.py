50c82be2ccd47a6c959d3d802546b7b4e5f383ca
Lucas Freitas <lucasfreitasr@outlook.com>
2023-08-15 18:04:35 -0300

    add novas aulas

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from contact import models
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display= 'id','first_name','last_name','phone',
+    ordering = 'id',
+    #list_filter = ('created_date','id','first_name')
+    search_fields = 'id','first_name','last_name',
+    list_per_page = 10
+    list_max_show_all = 200
+    list_editable = 'first_name', 'last_name'
--- contact/models.py
+++ contact/models.py
@@ -10,3 +10,6 @@ class Contact(models.Model):
     email = models.EmailField(max_length=250, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
