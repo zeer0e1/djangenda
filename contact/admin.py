63be1e3aa9a9e791052bbe52c4784fc066372d84
Lucas Ribeiro <lucasfreitasr@outlook.com>
2023-08-17 14:01:46 -0300

    criando o model de categoria para os contatos

--- contact/admin.py
+++ contact/admin.py
@@ -4,12 +4,19 @@ from django.contrib import admin
 
 from contact import models
 
+
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display= 'id','first_name','last_name','phone',
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'category',
     ordering = 'id',
-    #list_filter = ('created_date','id','first_name')
-    search_fields = 'id','first_name','last_name',
+    # list_filter = ('created_date','id','first_name')
+    search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 200
     list_editable = 'first_name', 'last_name'
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = 'id',
