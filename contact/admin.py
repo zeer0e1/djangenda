ad97d32087ac566961e732199f10054f7d7f763d
Lucas Ribeiro <lucasfreitasr@outlook.com>
2023-08-18 15:52:48 -0300

    criando a paginação da tela de contatos

--- contact/admin.py
+++ contact/admin.py
@@ -7,13 +7,13 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone', 'category',
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'category', 'show',
     ordering = 'id',
     # list_filter = ('created_date','id','first_name')
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = 'first_name', 'last_name'
+    list_editable = 'first_name', 'last_name', 'show',
 
 
 @admin.register(models.Category)
